b632999653a0204673b371835bc96ba7fb86087c
trankhanhlc <trankhanhlc99@gmail.com>
2022-05-26 01:09:02 +0700

    Build aab

---
+++ Assets/Logo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Logo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Test",2.2f);
+    }
+
+   private void Test()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainMenu");
+        CancelInvoke();
+    }
+}
